577d34021f34f05788f0bd89cc0882dec56c57d8
PiedraAndrick <ampiedra1@espe.edu.ec>
2022-05-25 17:04:36 -0500

    Revert "Revert "conf""
    This reverts commit 757a953deecd54b94d393a6a2bc6f60102bf619e.

--- Agente_PiedraAndrick.py
+++ Agente_PiedraAndrick.py
@@ -5,6 +5,12 @@ def airConditioning_world():
     estado_habitacion1 = input("Estado actual de la Habitacion 1 : ") #Ingreso del estado de la primera habitacion
     estado_habitacion2 = input("Estado actual de la Habitacion 2 : ") #Ingreso del estado de la segunda habitacion
     estado_habitacion3 = input("Estado actual de la Habitacion 3 : ") #Ingreso del estado de la tercera habitacion
+     #Creamos una lista de los estados iniciales
+    lista_habitaciones_estados_originales = [estado_habitacion1,estado_habitacion2,estado_habitacion3]
+    
+    #Definimos el estado global de las habitaciones, tomando los estados de las habitaciones ingresados.
+    estado_global = {'Habitacion 1': estado_habitacion1 , 'Habitacion 2': estado_habitacion2,'Habitacion 3': estado_habitacion3}
+    print("Estado Global:" + str(estado_global))
 
     #Definimos los estados finales de las habiaciones
     estadoActual_habitacion1 = input("Actualizar estado de habitacion 1: ") #Ingreso del estado requerido para la habitacion
