5b73a418a93eabe0e71ceffc563ceefb7eb467d5
PiedraAndrick <ampiedra1@espe.edu.ec>
2022-05-25 17:21:27 -0500

    variables_lista_estadosFinales

--- Agente_PiedraAndrick.py
+++ Agente_PiedraAndrick.py
@@ -12,4 +12,12 @@ def airConditioning_world():
     estado_global = {'Habitacion 1': estado_habitacion1 , 'Habitacion 2': estado_habitacion2,'Habitacion 3': estado_habitacion3}
     print("Estado Global:" + str(estado_global))
 
+    #Definimos los estados finales de las habiaciones
+    estadoActual_habitacion1 = input("Actualizar estado de habitacion 1: ") #Ingreso del estado requerido para la habitacion
+    estadoActual_habitacion2 = input("Actualizar estado de habitacion 2 : ") #Ingreso del estado requerido para la habitacion
+    estadoActual_habitacion3 = input("Actualizar estado de habitacion 3 : ") #Ingreso del estado requerido para la habitacion
+    
+    #Creamos la lista de los estados a los cuales se debe actualizar
+    lista_habitaciones_estados_actalizar = [estadoActual_habitacion1,estadoActual_habitacion2,estadoActual_habitacion3]
+
 airConditioning_world()
