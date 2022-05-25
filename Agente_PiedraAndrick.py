b4872772db9000802afc9a5dc3e2ccb60b58d963
PiedraAndrick <ampiedra1@espe.edu.ec>
2022-05-25 17:17:28 -0500

    Definicion_lista_estadoGlobal

--- Agente_PiedraAndrick.py
+++ Agente_PiedraAndrick.py
@@ -5,5 +5,11 @@ def airConditioning_world():
     estado_habitacion1 = input("Estado actual de la Habitacion 1 : ") #Ingreso del estado de la primera habitacion
     estado_habitacion2 = input("Estado actual de la Habitacion 2 : ") #Ingreso del estado de la segunda habitacion
     estado_habitacion3 = input("Estado actual de la Habitacion 3 : ") #Ingreso del estado de la tercera habitacion
+     #Creamos una lista de los estados iniciales
+    lista_habitaciones_estados_originales = [estado_habitacion1,estado_habitacion2,estado_habitacion3]
+    
+    #Definimos el estado global de las habitaciones, tomando los estados de las habitaciones ingresados.
+    estado_global = {'Habitacion 1': estado_habitacion1 , 'Habitacion 2': estado_habitacion2,'Habitacion 3': estado_habitacion3}
+    print("Estado Global:" + str(estado_global))
 
 airConditioning_world()
