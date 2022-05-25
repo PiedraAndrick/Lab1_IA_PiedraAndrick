e0c84a507f52fa39b7d36006bd2e25bc3d9fc53a
PiedraAndrick <ampiedra1@espe.edu.ec>
2022-05-25 17:00:14 -0500

    Create Agente_PiedraAndrick.py

---
+++ Agente_PiedraAndrick.py
@@ -0,0 +1,9 @@
+def airConditioning_world():
+#Costo inicial
+    costo = 0
+ #Definimos los estados iniciales de las habiaciones 
+    estado_habitacion1 = input("Estado actual de la Habitacion 1 : ") #Ingreso del estado de la primera habitacion
+    estado_habitacion2 = input("Estado actual de la Habitacion 2 : ") #Ingreso del estado de la segunda habitacion
+    estado_habitacion3 = input("Estado actual de la Habitacion 3 : ") #Ingreso del estado de la tercera habitacion
+
+airConditioning_world()
